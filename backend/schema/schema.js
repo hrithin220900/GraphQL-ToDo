fc257774d927fa9d320e9339c91f63fbac7a23f4
hrithin220900 <hrithink22@gmail.com>
2023-08-10 14:13:07 +0530

    Add date fields to user type

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -1,36 +1,36 @@
-
-
 const {
-    GraphQLObjectType,
-    GraphQLID,
-    GraphQLString,
-    GraphQLBoolean,
-    GraphQlDate,
-    GraphQLSchema,
-    GraphQLList,
-    GraphQLNonNull,
-    GraphQLEnumType,
-    GraphQLScalarType,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQlDate,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLEnumType,
+  GraphQLScalarType,
 } = require("graphql");
 
 const UserType = new GraphQLObjectType({
-    name: "Todo",
-    fields: () => ({
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        password: { type: GraphQLString },
-        todoList: { type: GraphQLList(GraphQLID) }
-    })
-})
+  name: "Todo",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    password: { type: GraphQLString },
+    todoList: { type: GraphQLList(GraphQLID) },
+    createdAt: { type: GraphQlDate },
+    updatedAt: { type: GraphQlDate },
+  }),
+});
 
 const TodoType = new GraphQLObjectType({
-    name: "Todo",
-    fields: () => ({
-        id: { type: GraphQLID },
-        title: { type: GraphQLString },
-        description: { type: GraphQLString },
-        completed: { type: GraphQLBoolean },
-        createdAt: { type: GraphQlDate },
-        updatedAt: { type: GraphQlDate }
-    })
-})
+  name: "Todo",
+  fields: () => ({
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    description: { type: GraphQLString },
+    completed: { type: GraphQLBoolean },
+    createdAt: { type: GraphQlDate },
+    updatedAt: { type: GraphQlDate },
+  }),
+});
