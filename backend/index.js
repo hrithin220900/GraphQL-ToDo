86c6ed1d2aae1514ce4099fc9abb091d04780191
hrithin220900 <hrithink22@gmail.com>
2023-08-10 12:58:51 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+const { graphqlHTTP } = require("express-graphql");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+
+app.listen(port, console.log(`Server running at http://localhost:${port}`));
