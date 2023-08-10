b805ae796c4e1ef39e7639b7ef0a5936cf7d870d
hrithin220900 <hrithink22@gmail.com>
2023-08-10 14:14:23 +0530

    fix:fix naming error

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -12,7 +12,7 @@ const {
 } = require("graphql");
 
 const UserType = new GraphQLObjectType({
-  name: "Todo",
+  name: "User",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
